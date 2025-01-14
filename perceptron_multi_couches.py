d3cb9723736715d913a1df309e4d721f07d41cce
Mahmoud <mahmoud.merheb84@gmail.com>
2025-01-14 16:44:33 +0100

    forward/back propagation

--- perceptron_multi_couches.py
+++ perceptron_multi_couches.py
@@ -7,7 +7,7 @@ class MLP:
     def __init__(self, n0, n1, n2, learning_rate=0.1, n_iter=1000):
         """
         Paramètres :
-        - n0 : nombre de neurones dans la couche d'entrée.
+        - n0 : nombre d'entrées.
         - n1 : nombre de neurones dans la couche cachée.
         - n2 : nombre de neurones dans la couche de sortie.
         - learning_rate : taux d'apprentissage.
@@ -35,4 +35,44 @@ class MLP:
             'b2': b2
         } 
     
+
+    def forward_propagation(X, self):
+        W1 = self.parametres['W1']
+        b1 = self.parametres['b1']
+        W2 = self.parametres['W2']
+        b2 = self.parametres['b2']
+
+        Z1 = W1.dot(X) + b1
+        A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = W2.dot(A1) + b2
+        A2 = 1 / (1 + np.exp(-Z2))
+
+        return {
+            'A1': A1,
+            'A2': A2
+        }
+    
+    def back_propagation(self, X, y, activations):
+        """Effectue la rétropropagation pour calculer les gradients."""
+        A1 = activations['A1']
+        A2 = activations['A2']
+        W2 = self.parametres['W2']
+
+        m = y.shape[1]
+
+        dZ2 = A2 - y
+        dW2 = 1 / m * dZ2.dot(A1.T)
+        db2 = 1 / m * np.sum(dZ2, axis=1, keepdims=True)
+
+        dZ1 = np.dot(W2.T, dZ2) * A1 * (1 - A1)
+        dW1 = 1 / m * dZ1.dot(X.T)
+        db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)
+
+        return {
+            'dW1': dW1,
+            'db1': db1,
+            'dW2': dW2,
+            'db2': db2
+        }
      
