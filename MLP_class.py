8b2d04b971439190780c6055d4dfe6c29499491e
Mahmoud <mahmoud.merheb84@gmail.com>
2025-01-16 15:58:01 +0100

    :sparkles: forward / back propagation

--- MLP_class.py
+++ MLP_class.py
@@ -21,8 +21,34 @@ class MLP:
 
         return parametres
     
+    def _forward_propagation(self, X):
+        activations = {'A0': X}
+        C = len(self.parametres) // 2
+
+        for c in range(1, C + 1):
+            Z = self.parametres['W' + str(c)].dot(activations['A' + str(c - 1)]) + self.parametres['b' + str(c)]
+            if c < C:  # ReLU pour toutes les couches sauf la dernière
+                activations['A' + str(c)] = np.maximum(0, Z)
+            else:  # Dernière couche : sigmoïde (classification binaire)
+                activations['A' + str(c)] = 1 / (1 + np.exp(-Z))
+
+        return activations
     
+    def _back_propagation(self, y, activations):
+        m = y.shape[1]
+        C = len(self.parametres) // 2
+
+        dZ = activations['A' + str(C)] - y
+        gradients = {}
+
+        for c in reversed(range(1, C + 1)):
+            gradients['dW' + str(c)] = 1 / m * np.dot(dZ, activations['A' + str(c - 1)].T)
+            gradients['db' + str(c)] = 1 / m * np.sum(dZ, axis=1, keepdims=True)
+            if c > 1:
+                dZ = np.dot(self.parametres['W' + str(c)].T, dZ) * (activations['A' + str(c - 1)] > 0)
+
+        return gradients
+
     
- 
 
 
