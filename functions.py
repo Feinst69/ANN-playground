9edc94b978a731e2b9560dd3670c576344efe80d
Feinst69 <157037115+Feinst69@users.noreply.github.com>
2025-01-16 17:30:30 +0300

    [update] creation of a python file for all the different class

---
+++ functions.py
@@ -0,0 +1,104 @@
+# .%%..%%..%%%%%%..%%%%%....%%%%....%%%%...%%...%%...%%%%...%%%%%...%%%%%%..%%.....
+# .%%.%%...%%......%%..%%..%%..%%..%%......%%%.%%%..%%..%%..%%..%%..%%......%%.....
+# .%%%%....%%%%....%%%%%...%%%%%%...%%%%...%%.%.%%..%%..%%..%%..%%..%%%%....%%.....
+# .%%.%%...%%......%%..%%..%%..%%......%%..%%...%%..%%..%%..%%..%%..%%......%%.....
+# .%%..%%..%%%%%%..%%..%%..%%..%%...%%%%...%%...%%...%%%%...%%%%%...%%%%%%..%%%%%%.
+# .................................................................................
+
+class KerasModel:
+    def __init__(self, input_dim, layers, activations, optimizer='adam', learning_rate=0.01):
+        """
+        Initialize the CustomModel class.
+
+        Parameters:
+        - input_dim: int, the number of input features.
+        - layers: list of int, the number of neurons in each layer.
+        - activations: list of str, the activation function for each layer.
+        - optimizer: str, the optimizer to use (default is 'adam').
+        - learning_rate: float, the learning rate for the optimizer (default is 0.001).
+        """
+        self.input_dim = input_dim
+        self.layers = layers
+        self.activations = activations
+        self.optimizer = optimizer
+        self.learning_rate = learning_rate
+        self.model = self.build_model()
+
+    def build_model(self):
+        """
+        Build the Sequential model based on the specified layers and activations.
+        """
+        model = Sequential()
+        
+        # Add the first layer with input dimension
+        model.add(Dense(self.layers[0], input_dim=self.input_dim, activation=self.activations[0]))
+        
+        # Add the remaining layers
+        for neurons, activation in zip(self.layers[1:], self.activations[1:]):
+            model.add(Dense(neurons, activation=activation))
+        
+        # Compile the model
+        if self.optimizer == 'adam':
+            optimizer = Adam(learning_rate=self.learning_rate)
+        else:
+            raise ValueError("Currently only 'adam' optimizer is supported.")
+        
+        model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+        
+        return model
+
+    def train(self, X_train, y_train, epochs=10, batch_size=32):
+        """
+        Train the model on the provided training data.
+
+        Parameters:
+        - X_train: array-like, the training data.
+        - y_train: array-like, the training labels.
+        - epochs: int, the number of epochs to train (default is 10).
+        - batch_size: int, the batch size for training (default is 32).
+        """
+        self.history = self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+
+    def evaluate(self, X_test, y_test):
+        """
+        Evaluate the model on the provided test data.
+
+        Parameters:
+        - X_test: array-like, the test data.
+        - y_test: array-like, the test labels.
+        """
+        return self.model.evaluate(X_test, y_test)
+
+    def predict(self, X):
+        """
+        Make predictions on the provided data.
+
+        Parameters:
+        - X: array-like, the data to make predictions on.
+        """
+        return self.model.predict(X)
+
+    def save_model(self, filename):
+        """
+        Save the model to a file.
+
+        Parameters:
+        - filename: str, the name of the file to save the model to.
+        """
+        self.model.save(filename)
+
+    def summary(self):
+        """
+        Print the summary of the model.
+        """
+        self.model.summary()
+
+    def load_model(self, filename):
+        """
+        Load a model from a file.
+
+        Parameters:
+        - filename: str, the name of the file to load the model from.
+        """
+        from keras.models import load_model
+        self.model = load_model(filename)
